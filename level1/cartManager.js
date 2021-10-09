6599a0a925d1ae4af743b49684700e56c2ea9890
Chakib BOUABD <cbouabd@nomadvantage.com>
2021-10-09 15:46:38 +0200

    WIP: sumCartPrices

--- level1/cartManager.js
+++ level1/cartManager.js
@@ -10,8 +10,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCartTotals = void 0;
+const sumCartPrices = (cart, articles) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield cart.items.reduce((a, b) => __awaiter(void 0, void 0, void 0, function* () {
+        const sumA = yield a;
+        const sumB = yield a;
+        return sumA + sumB;
+    }), Promise.resolve(0));
+});
 const getCartTotals = (data) => __awaiter(void 0, void 0, void 0, function* () {
     const { articles, carts } = data;
     const calculatedCart = { carts: [] };
+    for (const cart of carts) {
+        calculatedCart.carts.push({
+            id: cart.id,
+            total: 0,
+        });
+    }
 });
 exports.getCartTotals = getCartTotals;
--- level1/cartManager.ts
+++ level1/cartManager.ts
@@ -1,6 +1,22 @@
-import { JsonData, OutputType } from './types';
+import { JsonData, OutputType, CartType, ArticleType } from './types';
+
+const sumCartPrices = async (cart: CartType, articles: ArticleType[]): Promise<number> => {
+    return await cart.items.reduce(async (a, b) => {
+        const sumA = await a;
+        const sumB = await a;
+
+        return sumA + sumB;
+    }, Promise.resolve(0));
+};
 
 export const getCartTotals = async (data: JsonData) => {
     const { articles, carts } = data;
     const calculatedCart: OutputType = { carts: [] };
+
+    for (const cart of carts) {
+        calculatedCart.carts.push({
+            id: cart.id,
+            total: 0,
+        });
+    }
 };
